8502e681bc71eaa5f128c0de5fd4339f22b51f1a
kyogre <azizzchanou@gmail.com>
2023-12-27 00:58:40 +0100

    UPDATE

--- components/Header.vue
+++ components/Header.vue
@@ -6,7 +6,14 @@
   <header class="p-4 border border-slate dark:text-gray-800 ">
     <div class="container  flex justify-between h-16 mx-auto">
       <a rel="noopener noreferrer" href="#" aria-label="Back to homepage" class="flex items-center p-2">
-       <nuxt-img src="../assets/"/>
+        <!-- Quand tu utilise la balise img normale, cette nomenclature suffit -->
+       <img src="~/assets/images/home/logo.png"/>
+       <!-- Quand tu utilise NuxtImg ca devient complexe, j'ai du changer la ou pointe la racine vers assets/images
+      desormais tu mets directements / et tu suis le chemin normal vers ton images
+      dans l'example en bas on aurait eu normalement src="~/assets/images/home/logo.png" mais maintenant comme on pointe deja vers 
+      assets/images on fait seulement src="/home/logo.png"
+      -->
+       <NuxtImg src="/home/logo.png"/>
         <span class="ml-2">Explore a different way to travel</span>
       </a>
       <ul class="items-stretch hidden space-x-3 md:flex">
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,12 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/assets/css/style.css"],
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/image"],
+  modules: ["@nuxtjs/tailwindcss", "nuxt-swiper", "@nuxt/image"],
+  /**c'est un attribut de la librarie nuxt-image qui permet de referencer 
+   * les images vers le dossier assets/images au lieu de la racine qui est 'public'
+   * 
+   **/
+  image: {
+    dir: "assets/images",
+  },
 });
